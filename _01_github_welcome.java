241abe0f41d5367741549d0132aa8c8d27befcc1
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-02 09:14:02 +0900

    add world

--- _01_github_welcome.java
+++ _01_github_welcome.java
@@ -3,5 +3,6 @@ package github_01;
 public enum _01_github_welcome {
     public static void main(String[] args) {
         System.out.println("welcome");
+        System.out.println("world");
     }
 }

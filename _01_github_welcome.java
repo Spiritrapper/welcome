1a76ee817b3c7f05a4053e09f4448322607579df
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-02 12:44:34 +0900

    git desktop 여기서했나?

--- _01_github_welcome.java
+++ _01_github_welcome.java
@@ -1,10 +1,11 @@
 package github_01;
 
-public enum _01_github_welcome {
+public class _01_github_welcome {
     public static void main(String[] args) {
         System.out.println("welcome");
         System.out.println("world");
         System.out.println("이름");
         System.out.println("333333");
+        System.out.println("결국된건가?");
     }
 }

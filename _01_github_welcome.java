200be30826573ccec8419763d25655713c8047af
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-02 09:17:08 +0900

    이름 추가

--- _01_github_welcome.java
+++ _01_github_welcome.java
@@ -4,5 +4,6 @@ public enum _01_github_welcome {
     public static void main(String[] args) {
         System.out.println("welcome");
         System.out.println("world");
+        System.out.println("이름");
     }
 }

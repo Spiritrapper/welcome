3e2963dbb67cf62e0e6ff799ebbca951640670c8
Spiritrapper <134787232+Spiritrapper@users.noreply.github.com>
2023-06-02 15:19:18 +0900

    또들어가라

--- _01_github_welcome.java
+++ _01_github_welcome.java
@@ -9,5 +9,7 @@ public class _01_github_welcome {
         System.out.println("결국된건가?");
         System.out.println("여기는 vsc 프로그램");
         System.out.println("intellij ");
+        System.out.println("버전업로드");
+        System.out.println("여긴 vsc");
     }
 }
